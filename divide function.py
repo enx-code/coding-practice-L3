25c54b1dc8cd0360815f7b3c51921542c6585869
enx-code <77584605+enx-code@users.noreply.github.com>
2021-06-05 23:16:16 +0100

    Add files via upload

---
+++ divide function.py
@@ -0,0 +1,7 @@
+'''creating function'''
+def divide(a,b):
+    '''ordering which operation to follow'''
+    div_eq = a/b;
+    '''printing the result of function in shown format. user
+have to call function'''
+    print("{0} divide by {1} equals to {2}".format(a,b,div_eq))
