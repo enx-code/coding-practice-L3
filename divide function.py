81da447d35f360f691434447258d882d62d0b6aa
enx-code <77584605+enx-code@users.noreply.github.com>
2021-06-05 23:35:42 +0100

    Update divide function.py
    example added

--- divide function.py
+++ divide function.py
@@ -3,5 +3,5 @@ def divide(a,b):
     '''ordering which operation to follow'''
     div_eq = a/b;
     '''printing the result of function in shown format. user
-have to call function'''
+    have to call function example: divide(1,2)'''
     print("{0} divide by {1} equals to {2}".format(a,b,div_eq))
