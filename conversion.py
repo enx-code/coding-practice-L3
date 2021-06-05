f8d45782c882e6cf2286a5ed9a6555f469415fd7
enx-code <77584605+enx-code@users.noreply.github.com>
2021-06-05 23:32:53 +0100

    Update conversion.py
    this is the correct code for title

--- conversion.py
+++ conversion.py
@@ -1,1 +1,13 @@
 print('Hello World')
+
+'''Collect input in KG'''
+kg_input = input("Enter weight in KG:")
+
+'''Convert to float'''
+kg_input = float(kg_input);
+
+'''Convert to LB 1kg = 2.205lb'''
+lb_output = kg_input*2.205
+
+'''output print the value'''
+print("Weight converted to LB ",lb_output)
